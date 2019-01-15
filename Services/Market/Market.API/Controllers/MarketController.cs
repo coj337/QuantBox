7b71d90c557d84dd820af1409c2fdeba672b1647
Colin Watson <cojwatson@gmail.com>
2019-01-15 20:07:49 +1100

    Fixed API paths, added sqlserver container and made API communicate internally over HTTPS

--- Services/Market/Market.API/Controllers/MarketController.cs
+++ Services/Market/Market.API/Controllers/MarketController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Market.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class MarketController : ControllerBase
     {
