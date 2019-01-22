3d5dcf774cebed811ee517780a259ddea0a4efd3
Colin Watson <cojwatson@gmail.com>
2019-01-22 22:28:57 +1100

    Modified arbitrage algorithm to work down the orderbooks instead of using top

--- Services/Arbitrage/Arbitrage.Infrastructure/ArbitrageService.cs
+++ Services/Arbitrage/Arbitrage.Infrastructure/ArbitrageService.cs
@@ -51,6 +51,7 @@ namespace Arbitrage.Infrastructure
                     return; //Dont update anything if it fails
             }
 
+            Markets[marketData.Exchange][marketData.Data.Pair].Pair = marketData.Data.Pair;
             Markets[marketData.Exchange][marketData.Data.Pair].BaseCurrency = marketData.Data.BaseCurrency;
             Markets[marketData.Exchange][marketData.Data.Pair].AltCurrency = marketData.Data.AltCurrency;
             Markets[marketData.Exchange][marketData.Data.Pair].Asks = marketData.Data.Asks;
@@ -80,32 +81,31 @@ namespace Arbitrage.Infrastructure
 
         private decimal bestProfit = -100;
         private readonly List<ArbitrageResult> profitableTransactions = new List<ArbitrageResult>();
-        //TODO FOR FUTURE COLIN: Make the bid/asks work down the orderbook instead of using the top
         public void CheckExchangeForTriangleArbitrage(string exchange)
         {
             foreach (var market in Markets[exchange])
             {
                 //CheckMarketForTriangleArbitrage(exchange, market.Key);
-                //Loop every market except itself with the alt currency except itself //TODO: Optimize by starting at i=0 and 
+                //Loop every market except itself with the alt currency //TODO: Optimize by starting at i=0 and 
                 foreach (var market2 in Markets[exchange].Where(x => x.Key != market.Key && x.Value.AltCurrency == market.Value.AltCurrency || x.Value.BaseCurrency == market.Value.AltCurrency))
                 {
                     //TODO: Store all triangles so we can simply iterate through them instead of all this
                     MarketData finalMarket;
 
                     //TODO: Count fees in calculations
-                    var baseAmount = 100; //Assume we have 100 of the starting coin (BaseCurrency)
-                    var altAmount = baseAmount / market.Value.Asks.First().Price; //~3000 ETH or ~1126252 XRP from 100 BTC
+                    var baseAmount = 1; //Assume we have 100 of the starting coin (BaseCurrency)
+                    var altAmount = baseAmount / GetPriceQuote(exchange, market.Key, OrderbookType.Ask, baseAmount); //~3000 ETH or ~1126252 XRP from 100 BTC
 
                     decimal alt2Amount;
                     //If the alt bought in step 1 is still an alt, use bid price (e.g. we're selling to the new coin)
                     if (market2.Value.AltCurrency == market.Value.AltCurrency)
                     {
-                        alt2Amount = altAmount * market2.Value.Bids.First().Price; //~2988 ETH from 1126252 XRP (i.e. BTC->XRP->ETH->BTC)
+                        alt2Amount = altAmount * GetPriceQuote(exchange, market2.Key, OrderbookType.Bid, altAmount); //~2988 ETH from 1126252 XRP (i.e. BTC->XRP->ETH->BTC)
                         finalMarket = Markets[exchange].Values.FirstOrDefault(x => x.BaseCurrency == market.Value.BaseCurrency && x.AltCurrency == market2.Value.BaseCurrency);
                     }
                     else //Otherwise it's the base currency (e.g. we're buying to the new coin)
                     {
-                        alt2Amount = altAmount / market2.Value.Asks.First().Price; //~1129420 XRP from 3000 ETH (i.e. BTC->ETH->XRP->BTC)
+                        alt2Amount = altAmount / GetPriceQuote(exchange, market2.Key, OrderbookType.Ask, altAmount); //~1129420 XRP from 3000 ETH (i.e. BTC->ETH->XRP->BTC)
                         finalMarket = Markets[exchange].Values.FirstOrDefault(x => x.BaseCurrency == market.Value.BaseCurrency && x.AltCurrency == market2.Value.AltCurrency);
                     }
 
@@ -116,7 +116,7 @@ namespace Arbitrage.Infrastructure
                     }
 
                     //TODO: Will this sometimes be a buy instead?
-                    var finalAmount = alt2Amount * finalMarket.Bids.First().Price; //~100.3376728 BTC from 1129420 XRP
+                    var finalAmount = alt2Amount * GetPriceQuote(exchange, finalMarket.Pair, OrderbookType.Bid, altAmount); //~100.3376728 BTC from 1129420 XRP
                     decimal percentProfit = (finalAmount - baseAmount) / baseAmount * 100;
 
                     if (bestProfit < percentProfit) {
@@ -149,6 +149,50 @@ namespace Arbitrage.Infrastructure
                     var btcmCount = profitableTransactions.Count(x => x.Exchange == "BtcMarkets");
                 }
             }
+        }
+
+        public decimal GetPriceQuote(string exchange, string pair, OrderbookType type, decimal amount)
+        {
+            decimal price = 0;
+            List<Order> orders;
+
+            if (type == OrderbookType.Ask)
+            {
+                orders = Markets[exchange][pair].Asks;
+            }
+            else
+            {
+                orders = Markets[exchange][pair].Bids;
+            }
+
+            decimal amountLeft = amount;
+            int i = 0;
+
+            //Loop until our order's filled or we run out of orderbook
+            while (amountLeft > 0 && i < orders.Count())
+            {
+                decimal amountBought = orders[i].Amount;
+                if (orders[i].Amount > amountLeft) //Make sure we only partial fill the last of the order
+                {
+                    amountBought = amountLeft;
+                }
+
+                //Keep track of how much is left
+                amountLeft -= amountBought;
+
+                //Track average price payed for the amount filled
+                price = price + (orders[i].Price / (amount / amountBought));
+
+                i++;
+            }
+
+            return price;
+        }
+
+        public enum OrderbookType
+        {
+            Bid,
+            Ask
         }
 
         //Calculate arb chances for normal arb and pass it to the UI via SignalR
